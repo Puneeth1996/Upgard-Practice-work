c8100d03c6889a2364ec386ca61601db9af8700f
Puneeth1996 <cls>
2021-05-28 20:54:35 +0530

    moving to the next question

--- Sorting Algorith-1 and -2/interview question/sum_closest_zero.java
+++ Sorting Algorith-1 and -2/interview question/sum_closest_zero.java
@@ -1,10 +1,22 @@
 
 import java.util.Scanner;
+import java.util.Arrays;
 
-public class sum_closest_zero {
+public class sum_closest_zero1 {
+
     static void sumClosestToZero(int arr[], int n) {
-        // Write code here
-
+        Arrays.sort(arr);
+        int l = 0;
+        int r = n - 1;
+        int sum = arr[l] + arr[r];
+        while (l < r) {
+            if (sum < 0)
+                l++;
+            else if (sum > 0)
+                r--;
+            sum = arr[l] + arr[r];
+        }
+        System.out.println(sum + "" + arr[l] + "" + arr[r]);
     }
 
     public static void main(String[] args) {
