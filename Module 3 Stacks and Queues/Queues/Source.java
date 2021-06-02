79b42ca7212e3ff8974b39ab27e55afceda10655
Puneeth1996 <cls>
2021-06-02 21:38:11 +0530

    Completed all the modules

--- Module 3 Stacks and Queues/Queues/Source.java
+++ Module 3 Stacks and Queues/Queues/Source.java
@@ -2,17 +2,34 @@ import java.util.*;
 
 public class Source {
     public static void main(String args[]) {
-        Queue<Integer> queue = new LinkedList<Integer>();
+        Stack<Integer> stack = new Stack<>();
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         while (n-- > 0)
-            queue.add(s.nextInt());
-        deleteSecondHalf(queue);
+            stack.push(s.nextInt());
+        deleteFirstHalf(stack);
     }
 
-    // Method to delete the second half of the elements and print the remaining
-    // elements
-    static void deleteSecondHalf(Queue<Integer> queue) {
-        // Write your code here
+    // Method to delete the first half of the elements from the bottom of the
+    // stackand print the remaining elements
+    static void deleteFirstHalf(Stack<Integer> stack) {
+        int n = stack.size();
+        Stack<Integer> stack2 = new Stack<>();
+        List<Integer> list = new ArrayList<>();
+        int count = 0;
+        int k;
+        if (n % 2 == 0) {
+            k = n / 2;
+        } else {
+            k = n / 2 + 1;
+        }
+        while (count != k) {
+            stack2.add(stack.pop());
+            count++;
+        }
+        while (!stack2.isEmpty()) {
+            list.add(stack2.pop());
+        }
+        System.out.println(list);
     }
 }
