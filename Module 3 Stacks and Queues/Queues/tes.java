31d17f22755f00af1b4da7d50012f77567ec7433
Puneeth1996 <cls>
2021-06-02 13:51:59 +0530

    another problem test.java

--- Module 3 Stacks and Queues/Queues/tes.java
+++ Module 3 Stacks and Queues/Queues/tes.java
@@ -1,19 +1,29 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.*;
 
-class tes{
-    public static void main(String arg[]) {
-        Queue<Character> q=new LinkedList<>();
-        System.out.println(q.isEmpty());
-        q.add('a');
-        q.add('b');
-        q.add('c');
-        q.add('d');
-        System.out.println(q);
-        System.out.println(q.size());
-        System.out.println(q.peek());
-        System.out.println(q.remove());
-        System.out.println(q);
-        System.out.println(q.isEmpty());
+class tes {
+    public static void main(String args[]) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        while (n-- > 0)
+            queue.add(s.nextInt());
+        printFifthElementFromStart(queue);
+    }
+
+    // Method to print the fifth element from the head of the queue
+    static void printFifthElementFromStart(Queue<Integer> queue) {
+        // Write your code here
+        if (queue.size() > 5) {
+            queue.remove();
+            queue.remove();
+            queue.remove();
+            queue.remove();
+            System.out.println(queue.peek());
+        } else {
+            System.out.println("There are not enough elements in the queue");
+        }
+
     }
 }
