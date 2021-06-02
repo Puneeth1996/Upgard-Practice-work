7f34c978063eb07ebdc6aab693b4b4068c59ea04
Puneeth1996 <cls>
2021-06-02 20:37:48 +0530

    Pratice question 2 | Q1

--- Module 3 Stacks and Queues/Queues/Source.java
+++ Module 3 Stacks and Queues/Queues/Source.java
@@ -1,78 +1,18 @@
 import java.util.*;
 
 public class Source {
-    class Node {
-        char data;
-        Node next;
-
-        public Node(char new_data) {
-            data = new_data;
-            next = null;
-        }
+    public static void main(String args[]) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        while (n-- > 0)
+            queue.add(s.nextInt());
+        deleteSecondHalf(queue);
     }
 
-    Node top;
-
-    // Push and pop operations
-    public void push(char new_data) {
-        Node new_node = new Node(new_data);
-        if (top == null) {
-            top = new_node;
-        } else {
-            new_node.next = top;
-            top = new_node;
-        }
-    }
-
-    public Node pop() {
-        Node node = top;
-        top = top.next;
-        return node;
-    }
-
-    // prints contents of stack
-    public void display(Node node) {
-        Node temp = node;
-        while (temp != null) {
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
-    }
-
-    // Reverses the stack using simple
-    // linked list reversal logic.
-    public Node reverse() {
-        Node prev, cur, succ;
-        cur = prev = this.top;
-        cur = cur.next;
-        prev.next = null;
-        while (cur != null) {
-
-            succ = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = succ;
-        }
-        return prev;
-    }
-
-    public static void main(String[] args) {
-        Source obj = new Source();
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        for (int i = 0; i < n; i++) {
-            obj.push(in.next().charAt(0));
-        }
-
-        obj.display(obj.top);
-        System.out.println(" ");
-        // reverse
-        if (n > 0) {
-            Node temp = obj.reverse();
-            obj.display(temp);
-        } else {
-            System.out.println("the stack is empty");
-        }
-
+    // Method to delete the second half of the elements and print the remaining
+    // elements
+    static void deleteSecondHalf(Queue<Integer> queue) {
+        // Write your code here
     }
 }
