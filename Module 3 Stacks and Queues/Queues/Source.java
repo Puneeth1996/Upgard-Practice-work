ee504178ce7f6b398e6822a11d4575d920897525
Puneeth1996 <cls>
2021-06-02 17:43:02 +0530

    Duplicate parenthesis problem is done

--- Module 3 Stacks and Queues/Queues/Source.java
+++ Module 3 Stacks and Queues/Queues/Source.java
@@ -1,89 +1,53 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.*;
 
 class Source {
 
-    private static final int capacity = 3;
-    Character arr[] = new Character[capacity];
-    int size = 0;
-    int top = -1;
-    int rear = 0;
+    // Function to find duplicate parenthesis in a
+    // balanced expression
+    static String findDuplicateParenthesis(String s) {
+        // create a stack of characters
+        Stack<Character> Stack = new Stack<>();
 
-    public void enqueue(Character pushedElement) {
-        if (top < capacity - 1) {
-            top++;
-            arr[top] = pushedElement;
-        } else {
-            System.out.println("Overflow !");
-        }
+        // Iterate through the given expression
+        char[] str = s.toCharArray();
+        for (char ch : str) {
+            // if current character is close parenthesis ')'
+            if (ch == ')') {
+                // pop character from the stack
+                char top = Stack.peek();
+                Stack.pop();
 
-    }
-
-    public Character dequeue() {
-        Character c = arr[rear];
-        if (top >= rear) {
-            rear++;
-        } else {
-            System.out.println("Underflow !");
-        }
-        return c;
-    }
-
-    private LinkedList<Character> linklist = new LinkedList<Character>();
-
-    public void push(Character item) {
-        linklist.addFirst(item);
-    }
-
-    public Character pop() {
-        if (linklist.size() <= 0)
-            System.out.println("Empty Stack");
-        return linklist.removeFirst();
-    }
-
-    public Character top() {
-        return linklist.getFirst();
-    }
-
-    boolean test(String candidate) {
-        char ch;
-        int length = candidate.length(), numLetters = 0, charCount;
-
-        char fromStack, fromQueue;
-        boolean stillPalindrome;
-
-        for (int i = 0; i < length; i++) {
-            ch = candidate.charAt(i);
-            if (Character.isLetter(ch)) {
-                numLetters++;
-                ch = Character.toLowerCase(ch);
-                push(ch);
-                enqueue(ch);
+                // stores the number of characters between a
+                // closing and opening parenthesis
+                // if this count is less than or equal to 1
+                // then the brackets are redundant else not
+                int elementsInside = 0;
+                while (top != '(') {
+                    elementsInside++;
+                    top = Stack.peek();
+                    Stack.pop();
+                }
+                if (elementsInside < 1) {
+                    return "Input string contains duplicate parenthesis";
+                }
+            } // push open parenthesis '(', operators and
+              // operands to stack
+            else {
+                Stack.push(ch);
             }
         }
 
-        stillPalindrome = true;
-        charCount = 0;
-        while (stillPalindrome && (charCount < numLetters)) {
-            fromStack = (Character) top();
-            pop();
-            fromQueue = dequeue();
-            if (fromStack != fromQueue)
-                stillPalindrome = false;
-            charCount++;
-        }
-
-        return stillPalindrome;
+        // No duplicates found
+        return "Input string does not contain duplicate parenthesis";
     }
 
     public static void main(String[] args) {
         Source obj = new Source();
+        String inputString = new String();
         Scanner in = new Scanner(System.in);
-        String inputString = in.nextLine();
-        if (obj.test(inputString))
-            System.out.println("'" + inputString + "' is a palindrome.");
-        else
-            System.out.println("'" + inputString + "' is NOT a palindrome.");
+        inputString = in.nextLine();
+        System.out.println(obj.findDuplicateParenthesis(inputString));
+
     }
+
 }
