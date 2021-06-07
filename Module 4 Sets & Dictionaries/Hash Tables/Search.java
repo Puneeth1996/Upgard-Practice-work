8cea08471043a6ca9748f2bcc474e53f6adef929
Puneeth1996 <cls>
2021-06-07 13:32:19 +0530

    practice question

--- Module 4 Sets & Dictionaries/Hash Tables/Search.java
+++ Module 4 Sets & Dictionaries/Hash Tables/Search.java
@@ -1,24 +1,22 @@
 import java.util.*;
 
-public class Search {
+class Search {
     public static void main(String[] args) {
-        String[] emails = { "chandler.bing@xyz.com", "ross.geller@xyz.com", "rachel.greene@xyz.com",
-                "joey.tribbiani@xyz.com", "monica.geller@xyz.com", "phoebe.buffay@xyz.com", "sheldon.copper@xyz.com",
-                "marie.george@xyz.com" };
+        Hashtable<String, Integer> emails = new Hashtable<String, Integer>();
+        emails.put("chandler.bing@xyz.com", 1);
+        emails.put("ross.geller@xyz.com", 2);
+        emails.put("rachel.greene@xyz.com", 3);
+        emails.put("joey.tribbiani@xyz.com", 4);
+        emails.put("monica.geller@xyz.com", 5);
+        emails.put("phoebe.buffay@xyz.com", 6);
+        emails.put("sheldon.copper@xyz.com", 7);
+        emails.put("marie.george@xyz.com", 8);
 
         String NewEmail = "monica.geller@xyz.com";
 
-        // Write your code here
-        System.out.println(binarySearch(emails, NewEmail, emails.length));
-    }
-
-    // Returns index of x if it is present in arr[],
-    // else return -1
-    public static int binarySearch(String arr[], String x, int n) {
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == x)
-                return i;
-        }
-        return -1;
+        if (emails.containsKey(NewEmail)) {
+            System.out.println(emails.get(NewEmail));
+        } else
+            System.out.println("False");
     }
 }
---
+++ Module 4 Sets & Dictionaries/Hash Tables/prac.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+
+class prac {
+    public static void main(String[] args) {
+        Hashtable<Integer, String> boardingPass = new Hashtable<Integer, String>();
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int arr[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        for (int i : arr) {
+        }
+    }
+}
