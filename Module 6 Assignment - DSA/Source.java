3e09b695e7ca00cdbaa131e62842e3615c649b97
Puneeth1996 <puneeth1996@gmail.com>
2021-06-24 14:14:28 +0530

    Find Itinerary in Order

--- Module 6 Assignment - DSA/Source.java
+++ Module 6 Assignment - DSA/Source.java
@@ -2,49 +2,42 @@ import java.util.*;
 
 public class Source {
 
-    public static void printElementInAllRows( int mat[][], int row, int col )
-    {   
-        int[] arr = new int[row];
-        int count  = 0;
-        Map<Integer,Integer> mp = new HashMap<>();
-        for (int j = 0; j < col; j++)
-            mp.put(mat[0][j],1);
-        for (int i = 1; i < row; i++)
+    public static void findItinerary(Map<String, String> dataSet)
+    {
+        Map<String, String> reverseMap = new HashMap<String, String>();
+        for (Map.Entry<String,String> entry: dataSet.entrySet())
+            reverseMap.put(entry.getValue(), entry.getKey());
+        String start = null;
+        for (Map.Entry<String,String> entry: dataSet.entrySet())
         {
-            for (int j = 0; j < col; j++)
+            if (!reverseMap.containsKey(entry.getKey()))
             {
-                if (mp.get(mat[i][j]) != null && mp.get(mat[i][j]) == i)
-                {
-                    mp.put(mat[i][j], i + 1);
-                    if (i == row - 1){
-                        count++;
-                        arr[count] = mat[i][j];
-                    }
-                }
+                start = entry.getKey();
+                break;
             }
         }
-        Arrays.sort(arr);
-        for (int i = 0; i < arr.length; i++){
-            if(arr[i]>0){
-                System.out.print(arr[i] + " ");
-            }            
+        if (start == null)
+        {
+        System.out.println("Invalid Input");
+        return;
         }
-
+        String to = dataSet.get(start);
+        while (to != null)
+        {
+            System.out.print(start +  "->" + to + "\n");
+            start = to;
+            to = dataSet.get(to);
+        }
     }
 
     public static void main(String[] args) {
-        Scanner sc =  new Scanner(System.in);
-        int row = sc.nextInt();
-        int col = sc.nextInt();
-        
-        int matrix[][] = new int[row][col];
-        for(int i = 0 ; i < row ; i++){
-            for(int j = 0 ; j < col ; j++){
-                matrix[i][j] = sc.nextInt();
-            }
+        Map<String, String> tickets = new HashMap<String, String>();
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+        for(int i = 0 ; i < n ; i++){
+            tickets.put(sc.next(),sc.next());
         }
-
-        printElementInAllRows(matrix, row, col);
+        findItinerary(tickets);
         sc.close();
     }
 }
