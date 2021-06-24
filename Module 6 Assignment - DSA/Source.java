19669e6f1c1c0b2a8b8a4e8166539fd665cb1e8d
Puneeth1996 <puneeth1996@gmail.com>
2021-06-24 08:35:05 +0530

    Sort Nearly Sorted Array

--- Module 6 Assignment - DSA/Source.java
+++ Module 6 Assignment - DSA/Source.java
@@ -1,46 +1,49 @@
 import java.util.*;
 
 public class Source {
-    static PriorityQueue<Integer> min;
-    static int k;
-    
-    static List<Integer> getAllKthNumber(int arr[])
+
+    private static void sortArray(int[] arr, int n, int k)
     {
-        List<Integer> list = new ArrayList<>();
-        for (int val : arr) {
-            if (min.size() < k)
-                min.add(val);
-            else {
-                if (val > min.peek()) {
-                min.poll();
-                min.add(val);
-                }
-            }
-            if (min.size() >= k)
-                list.add(min.peek());
-            else
-                list.add(-1);
+
+        // min heap
+        PriorityQueue<Integer> priorityQueue
+            = new PriorityQueue<>();
+
+        // add first k + 1 items to the min heap
+        for (int i = 0; i < k + 1; i++) {
+            priorityQueue.add(arr[i]);
         }
-        return list;
+
+        int index = 0;
+        for (int i = k + 1; i < n; i++) {
+            arr[index++] = priorityQueue.peek();
+            priorityQueue.poll();
+            priorityQueue.add(arr[i]);
+        }
+
+        Iterator<Integer> itr = priorityQueue.iterator();
+
+        while (itr.hasNext()) {
+            arr[index++] = priorityQueue.peek();
+            priorityQueue.poll();
+        }
     }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        k = sc.nextInt();
-        int stream[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            stream[i] = sc.nextInt();
+        int k = sc.nextInt();
+        int arr[] = new int[n];
+        
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        sortArray(arr, n, k);
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         sc.close();
-        min = new PriorityQueue<>();
-        List<Integer> res = getAllKthNumber(stream);
-        for (int x : res){
-            if(x == -1){
-                System.out.println("None");
-            }
-            else{
-                System.out.println( k + " largest number is " + x);
-            }
-        }
     }
 }
