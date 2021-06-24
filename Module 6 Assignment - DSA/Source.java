4932a450198228fd7f8745a17df716bc90a77bda
Puneeth1996 <puneeth1996@gmail.com>
2021-06-24 14:30:32 +0530

    Find Median After Merging Two Sorted Arrays

--- Module 6 Assignment - DSA/Source.java
+++ Module 6 Assignment - DSA/Source.java
@@ -2,30 +2,59 @@ import java.util.*;
 
 public class Source {
 
-    public static int search(int arr[], int t)
-    {
-        ArrayList<Integer> clist = new ArrayList<>();
-
-        for (int i : arr)
-            clist.add(i);
-
-        return clist.indexOf(t);
+    public static int median(int ar1[], int ar2[], int n)
+    {  
+        int i = 0; 
+        int j = 0;
+        int count;
+        int m1 = -1, m2 = -1;
+    
+        for (count = 0; count <= n; count++)
+        {
+            if (i == n)
+            {
+                m1 = m2;
+                m2 = ar2[0];
+                break;
+            }
+            else if (j == n)
+            {
+                m1 = m2;
+                m2 = ar1[0];
+                break;
+            }
+            if (ar1[i] <= ar2[j])
+            {  
+                m1 = m2; 
+                m2 = ar1[i];
+                i++;
+            }
+            else
+            {
+                m1 = m2; 
+                m2 = ar2[j];
+                j++;
+            }
+        }
+    
+        return (m1 + m2)/2;
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int arr[] = new int[n];
+        
+        int arr1[] = new int[n];
+        int arr2[] = new int[n];
+
         for(int i = 0 ; i < n ; i++){
-            arr[i] = sc.nextInt();
+            arr1[i] = sc.nextInt();
         }
-        int key = sc.nextInt();
-        int i = search(arr, key);
-        if (i != -1) {
-            System.out.println(i);
-        } else {
-            System.out.println("-1");
+        
+        for(int i = 0 ; i < n ; i++){
+            arr2[i] = sc.nextInt();
         }
+        System.out.println(median(arr1, arr2, n));
         sc.close();
     }
 }
